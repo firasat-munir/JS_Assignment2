96e241d995e3f2551b63652f0f7e26e10f8b5dfc
firasatmunir <zapp3r1997@gmail.com>
2022-07-01 12:24:12 +0500

    updated task5

--- task5.js
+++ task5.js
@@ -5,3 +5,10 @@ if (guess==number){
     document.write("Bingo! Correct answer.")
 }
 
+else if(guess==number+1){
+    document.write("Close enough to correct answer")
+}
+
+else{
+    document.write("Wrong Answer")
+}
